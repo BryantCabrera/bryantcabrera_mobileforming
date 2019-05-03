600feae28f96da057b802a507e7889190e6f2768
Bryant <cabrera.bryant@gmail.com>
2019-05-03 11:14:14 -0700

    Bryant updates iOS and Android files to allow for use of react-native-navigation.

--- android/app/src/main/java/com/bryantcabrera_mobileforming/MainActivity.java
+++ android/app/src/main/java/com/bryantcabrera_mobileforming/MainActivity.java
@@ -1,15 +1,21 @@
 package com.bryantcabrera_mobileforming;
 
-import com.facebook.react.ReactActivity;
+// import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
+// Updated for ReactNative Navigation
+public class MainActivity extends NavigationActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "bryantcabrera_mobileforming";
-    }
 }
+
+// public class MainActivity extends ReactActivity {
+
+//     /**
+//      * Returns the name of the main component registered from JavaScript.
+//      * This is used to schedule rendering of the component.
+//      */
+//     @Override
+//     protected String getMainComponentName() {
+//         return "bryantcabrera_mobileforming";
+//     }
+// }
