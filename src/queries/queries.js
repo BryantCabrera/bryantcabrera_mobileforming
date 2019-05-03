ef6a14cc8f58c4e4c68bdc58f52701a566359a2d
Bryant <cabrera.bryant@gmail.com>
2019-05-03 16:50:45 -0700

    Bryant creates queries/queries.js with createReservation mutation.

---
+++ src/queries/queries.js
@@ -0,0 +1,18 @@
+import gql from "graphql-tag";
+
+// 1st line makes sure right value is passed
+//the $ denotes a variable, and we are saying that variable must look like whatever comes after the : (defined in our server schema)
+//this input variable will be called in the createUser mutation
+//the 2nd object is the response we want back from the database
+
+export const createReservation = gql`
+    mutation createReservation($data: ReservationCreateInput!) {
+        createReservation(data: $data) {
+            id
+            name
+            hotelName
+            arrivalDate
+            departureDate
+        }
+    }
+`
