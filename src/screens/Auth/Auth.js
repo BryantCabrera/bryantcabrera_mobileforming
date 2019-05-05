3914fabc993c71905ef2a120ac67ff794a65406e
Bryant <cabrera.bryant@gmail.com>
2019-05-05 10:08:46 -0700

    Bryant creates Auth.js state and functions.

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -14,16 +14,140 @@ import {
     TouchableWithoutFeedback,
     ActivityIndicator
 } from 'react-native';
+
+import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
+import HeadingText from "../../components/UI/HeadingText/HeadingText";
+import MainText from "../../components/UI/MainText/MainText";
+import ButtonWithBackground from "../../components/UI/ButtonWithBackground/ButtonWithBackground";
+import validate from "../../utility/validation";
 import startMainTabs from '../MainTabs/startMainTabs';
+import backgroundImage from "../../assets/background.jpg";
 
 class AuthScreen extends Component {
+    state = {
+        viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape",
+        authMode: "login",
+        controls: {
+            email: {
+                value: "",
+                valid: false,
+                validationRules: {
+                    isEmail: true
+                },
+                touched: false
+            },
+            password: {
+                value: "",
+                valid: false,
+                validationRules: {
+                    minLength: 6
+                },
+                touched: false
+            },
+            confirmPassword: {
+                value: "",
+                valid: false,
+                validationRules: {
+                    equalTo: "password"
+                },
+                touched: false
+            }
+        },
+        isLoading: false
+    };
+
+    constructor(props) {
+        super(props);
+        Dimensions.addEventListener("change", this.updateStyles);
+    }
+
+    
+    componentWillUnmount() {
+        // Modularized the function to remove eventListener so that we could call it in this life cycle hook
+        Dimensions.removeEventListener("change", this.updateStyles);
+
+        startMainTabs();
+    }
+    
     componentDidMount() {
         
     }
 
-    componentWillMount() {
-        startMainTabs();
+    // Toggles between signup and login
+    switchAuthModeHandler = () => {
+        this.setState(prevState => {
+            return {
+                authMode: prevState.authMode === "login" ? "signup" : "login"
+            };
+        });
+    };
+
+    // Modularized this function to prevent memory leaks from not detaching event listener
+    updateStyles = (dims) => {
+        this.setState({
+            viewMode:
+                dims.window.height > 500 ? "portrait" : "landscape"
+        });
     }
+
+    // Validation
+        // Key is the property key in state
+        // don't use this.state syntax because we only want to update the key of the particular property, so use prevState
+        // **To check if this works, check Auth.js state in debugger
+    updateInputState = (key, value) => {
+        let connectedValue = {};
+
+        // Checks if we have the .equalTo rule
+        if (this.state.controls[key].validationRules.equalTo) {
+            const equalControl = this.state.controls[key].validationRules.equalTo;
+
+            const equalValue = this.state.controls[equalControl].value;
+
+            connectedValue = {
+                ...connectedValue,
+                equalTo: equalValue
+            };
+        }
+
+        if (key === "password") {
+            connectedValue = {
+                ...connectedValue,
+                equalTo: value
+            };
+        }
+
+        this.setState(prevState => {
+            return {
+                controls: {
+                    // ERROR: make sure when you populate prevstate, you type …prevState.propertyNameForObjectInState not …prevState because the latter will override the state for that object
+                    ...prevState.controls,
+                    // want to hard code and also check confirmPassword because if the user later changed just the password input, it still returned true for passwords matching
+                    // make sure this confirmPassword comes 1st to avoid overriding the password check
+                    confirmPassword: {
+                        ...prevState.controls.confirmPassword,
+                        valid:
+                            key === "password"
+                                ? validate(
+                                    prevState.controls.confirmPassword.value,
+                                    prevState.controls.confirmPassword.validationRules,
+                                    connectedValue
+                                )
+                                : prevState.controls.confirmPassword.valid
+                    },
+                    [key]: {
+                        ...prevState.controls[key],
+                        value: value,
+                        valid: validate(
+                            value,
+                            prevState.controls[key].validationRules,
+                            connectedValue
+                        ),
+                        touched: true
+                    }
+                }
+            };
+        });
+    };
 
     render() {
         return (
@@ -33,5 +157,38 @@ class AuthScreen extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    backgroundImage: {
+        width: "100%",
+        flex: 1
+    },
+    inputContainer: {
+        width: "80%"
+    },
+    input: {
+        backgroundColor: "#eee",
+        borderColor: "#bbb"
+    },
+    landscapePasswordContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    portraitPasswordContainer: {
+        flexDirection: "column",
+        justifyContent: "flex-start"
+    },
+    landscapePasswordWrapper: {
+        width: "45%"
+    },
+    portraitPasswordWrapper: {
+        width: "100%"
+    }
+});
 
 export default AuthScreen;
