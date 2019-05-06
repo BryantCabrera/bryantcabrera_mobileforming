0ddfa373466dcb4c548d678053b6948a120c8bc4
Bryant <cabrera.bryant@gmail.com>
2019-05-06 09:08:39 -0700

    Bryant adds logout functionality and styling to SideDrawer.js.

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,30 +1,58 @@
-import React, { Component } from 'react';
-import {
-    View,
-    Text,
-    Button,
-    TextInput,
-    StyleSheet,
-    ImageBackground,
-    Dimensions,
-    KeyboardAvoidingView,
-    Keyboard,
-    TouchableWithoutFeedback,
-    ActivityIndicator
-} from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity, Platform } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import App from "../../../App";
 
-class SideDrawerScreen extends Component {
-    componentDidMount() {
+class SideDrawer extends Component {
+    logout = () => {
+        // this.props.navigator.push({
+        //     screen: "bryant-mobileforming.AuthScreen"
+        // });
 
+        App();
     }
 
     render() {
         return (
-            <Text>
-                This is SideDrawer.
-            </Text>
-        )
+            <View
+                style={[
+                    styles.container,
+                    // Dimensions is a helper object that let’s us find out the dimensions of the device we’re running on
+                    // Gets the window's dimensions
+                    { width: Dimensions.get("window").width * 0.8 }
+                ]}
+            >
+                <TouchableOpacity onPress={this.logout}>
+                    <View style={styles.drawerItem}>
+                        <Icon
+                            name={Platform.OS === "android" ? "md-log-out" : "ios-log-out"}
+                            size={30}
+                            color="#aaa"
+                            style={styles.drawerItemIcon}
+                        />
+                        <Text>Sign Out</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        );
     }
 }
 
-export default SideDrawerScreen;
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 50,
+        backgroundColor: "white",
+        flex: 1
+    },
+    drawerItem: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 10,
+        backgroundColor: "#eee"
+    },
+    drawerItemIcon: {
+        marginRight: 10
+    }
+});
+
+export default SideDrawer;
